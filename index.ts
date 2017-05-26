80689273a014a6b0f8d41a825a77b3f39fb3daed
Paul Irish <paul.irish@gmail.com>
2017-05-26 16:12:07 -0700

    exponential dampening. responds to touchMove. bigger.

--- index.ts
+++ index.ts
@@ -1,18 +1,26 @@
 const img = new Image;
 const canvas = document.querySelector('canvas') as HTMLCanvasElement;
-const velocity = {r: 0, rotationVelocity: 0, maxVelocity: 100};
-const friction = -0.001;
+const velocity = { r: 0, rotationVelocity: 0, maxVelocity: 100 };
 
-const imgDimensions = {width: 200, height: 200};
-const touchInfo : {
-  rateSamples: number;
+interface Sample {
+  xDistance: number;
+  duration: number;
+};
+
+const imgDimensions = { width: 300, height: 300 };
+const touchInfo: {
   startX?: number;
   startY?: number;
   lastX?: number;
   lastY?: number;
-} = {rateSamples: 0};
+  samples: Sample[];
+  lastTimestamp?: number;
+} = { samples: [] };
 
 const dPR = window.devicePixelRatio;
+let timeRemaining = 5000;
+let lastTouchEnd: number;
+let lastTouchVelocity: number;
 
 canvas.height = imgDimensions.height * dPR;
 canvas.width = imgDimensions.width * dPR;
@@ -20,10 +28,12 @@ canvas.style.width = `${imgDimensions.width}px`;
 canvas.style.height = `${imgDimensions.height}px`;
 
 const ctx = canvas.getContext('2d')!;
+const fontHeight = 20;
+ctx.font = `${fontHeight * dPR}px 'Roboto'`;
 
 async function boot() {
   return new Promise((res) => {
-    img.onload = function() {
+    img.onload = function () {
       res();
     }
 
@@ -36,21 +46,29 @@ function paint() {
   ctx.save();
   ctx.translate(dPR * imgDimensions.width / 2, dPR * imgDimensions.height / 2);
   ctx.rotate(velocity.r);
-  ctx.translate(-100 * dPR, -100 * dPR);
+  ctx.translate(-150 * dPR, -150 * dPR);
   ctx.drawImage(img, 0, 0, imgDimensions.width * dPR, imgDimensions.height * dPR);
   ctx.restore();
+
+  const velocityText = Math.abs(velocity.rotationVelocity * 100).toLocaleString(undefined, { maximumFractionDigits: 2 });
+  const turnsText = (velocity.r / Math.PI).toLocaleString(undefined, { maximumFractionDigits: 1 });
+  ctx.fillText(`turns: ${turnsText}`, 0, fontHeight);
+  ctx.fillText(`velocity: ${velocityText}`, 0, canvas.height * dPR - fontHeight);
 }
+
+const easeOutQuad = (t: number) => t * (2 - t);
 
 function tick() {
   requestAnimationFrame(() => {
     velocity.r += velocity.rotationVelocity;
 
-    if (velocity.rotationVelocity > friction && velocity.rotationVelocity > 0) {
-      velocity.rotationVelocity += friction;
-    }
-
-    if (velocity.rotationVelocity < friction && velocity.rotationVelocity < 0) {
-      velocity.rotationVelocity -= friction;
+    if (lastTouchEnd) {
+      const timeSinceLastTouch = Date.now() - lastTouchEnd;
+      const timeLeftPct = timeSinceLastTouch / timeRemaining;
+      if (timeLeftPct < 1) {
+        const newVelocity = lastTouchVelocity - (easeOutQuad(timeLeftPct) * lastTouchVelocity);
+        velocity.rotationVelocity = newVelocity;
+      }
     }
 
     paint();
@@ -61,26 +79,53 @@ function tick() {
 function onTouchStart(e: TouchEvent) {
   touchInfo.startX = e.touches[0].clientX;
   touchInfo.startY = e.touches[0].clientX;
+  touchInfo.lastTimestamp = e.timeStamp;
 }
 
 function onTouchMove(e: TouchEvent) {
   touchInfo.lastX = e.touches[0].clientX;
   touchInfo.lastY = e.touches[0].clientY;
-  touchInfo.rateSamples++;
+
+  touchInfo.samples.push({
+    xDistance: touchInfo.lastX - touchInfo.startX!,
+    duration: e.timeStamp - touchInfo.lastTimestamp!
+  });
+
+  if (touchInfo.samples.length >= 3) {
+    updateVelocity(touchInfo.samples);
+    touchInfo.samples = [];
+  }
+
+  touchInfo.startX = touchInfo.lastX;
+  touchInfo.startY = touchInfo.lastY;
+  touchInfo.lastTimestamp = e.timeStamp;
 }
 
 function touchEnd() {
-  if (touchInfo.lastX !== undefined && touchInfo.lastY !== undefined) {
-    let touchSpeed = ((touchInfo.lastX! - touchInfo.startX!) / window.innerWidth) / touchInfo.rateSamples;
+  updateVelocity(touchInfo.samples);
+  touchInfo.samples = [];
+}
 
-    if (velocity.rotationVelocity < velocity.maxVelocity) {
-      velocity.rotationVelocity -= touchSpeed;
-    }
+
+function updateVelocity(samples: Sample[]) {
+  const multiplier = 25;
+
+  const totalDistance = samples.reduce((total, curr) => total += curr.xDistance, 0);
+  const totalDuration = samples.reduce((total, curr) => total += curr.duration, 0);
+  const touchSpeed = totalDistance / totalDuration / multiplier;
+
+  if (!Number.isFinite(touchSpeed)) return;
+
+  if (Math.abs(velocity.rotationVelocity) < velocity.maxVelocity) {
+    velocity.rotationVelocity -= touchSpeed;
   }
 
-  touchInfo.lastX = undefined;
-  touchInfo.lastY = undefined;
-  touchInfo.rateSamples = 0;
+  resetLastTouch();
+}
+
+function resetLastTouch() {
+  lastTouchEnd = Date.now();
+  lastTouchVelocity = velocity.rotationVelocity;
 }
 
 
