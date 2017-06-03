ce9909e6cdb565d42c729715b84b1ac0456f1315
Sam Saccone <samccone@gmail.com>
2017-06-03 10:01:53 -0700

    twreak

--- ship.sh
+++ ship.sh
@@ -3,7 +3,8 @@ set -o pipefail
 
 yarn run build && \
 git init && \
+git checkout -b to_deploy
 git add . && \
 git commit -m "deploy" && \
 git remote add deploy_time https://${GH_TOKEN}@github.com/samccone/fidgetspin.xyz.git
-git push -f deploy_time master:gh-pages
+git push -f deploy_time to_deploy:gh-pages
